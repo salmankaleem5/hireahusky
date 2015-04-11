20884ca186099fd2df96bd250976823a9dff173e
Daanish R Zaidi <daanish.zaidi@uconn.edu>
2015-04-11 11:53:58 -0400

    i had a random merge conflict

---
+++ templates/login.php
@@ -0,0 +1,28 @@
+<?php
+if( isset($_POST['username']) && isset($_POST['password']) ){
+	require('../lib/database.php');
+}
+//require($this->data['baseUrl'].'/lib/database.php');
+include('header.php');
+?>
+
+<div id="main" class="row">
+	<div id="login-box" class="col-md-6 col-md-offset-3">
+		<h2>Sign in to get started</h2>
+		<form action="<?php echo $this->data['baseUrl'].'/login' ?>" method="POST">
+			<div class="form-group">
+				<input type="text" name="username" class="form-control" placeholder="Enter your username">
+			</div>
+			<div class="form-group">
+				<input type="password" name="password" class="form-control" placeholder="Enter your password">
+			</div>
+			<input type="hidden" name="login" value=""> <!--value will be 1 if logging in-->
+			<p><button id="login_button" type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button></p>
+			<p>Forgot your password? | Need to sign up?</p>
+		</form>
+	</div>
+</div>
+
+<?php 
+include('footer.php');
+?>
