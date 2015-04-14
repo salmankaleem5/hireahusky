2bd6c955089104fc8f1128f502b13a7dd97dc007
Salman <contact@salmankaleem.com>
2015-04-14 15:50:46 -0400

    More login authentication fixes

--- lib/database.php
+++ lib/database.php
@@ -4,7 +4,7 @@ if( isset($mysql) ){
 	return; //don't need to redefine if its already set
 }
 
-$mysql = new mysqli('localhost','root','','test');
+$mysql = new mysqli('localhost','root','','team3');
 if ($mysql->connect_errno) {
     die(sprintf("Failed to connect to MySQL: (%s) %s",$mysql->connect_errno, $mysql->connect_error));
 }
--- templates/login.php
+++ templates/login.php
@@ -5,16 +5,15 @@ include('header.php');
 <div id="main" class="row">
 	<div id="login-box" class="col-md-6 col-md-offset-3">
 		<h2>Sign in to get started</h2>
-		<form action="<?php echo $this->data['baseUrl'].'/login' ?>" method="POST">
+		<form action="<?php echo $this->data['baseUrl'].'/login'; ?>" method="POST">
 			<div class="form-group">
 				<input type="text" name="username" class="form-control" placeholder="Enter your username">
 			</div>
 			<div class="form-group">
 				<input type="password" name="password" class="form-control" placeholder="Enter your password">
 			</div>
-			<input type="hidden" name="login" value=""> <!--value will be 1 if logging in-->
 			<p><input id="login_button" type="submit" class="btn btn-primary btn-lg btn-block" value="Sign in"></p>
-			<p>Forgot your password? | Need to sign up?</p>
+			<p>Forgot your password? | <a href="<?php echo $this->data['baseUrl'].'/signup'; ?>">Need to sign up?</a></p>
 		</form>
 	</div>
 </div>
