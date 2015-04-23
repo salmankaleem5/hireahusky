cdce82d5d770f98c7e0f8bf45ebf0395b63e3697
Daanish R Zaidi <daanish.zaidi@uconn.edu>
2015-04-23 15:43:55 -0400

    added second search box for location

--- templates/index.php
+++ templates/index.php
@@ -4,9 +4,30 @@ include('header.php');
 
 <div class="jumbotron">
   <h1>Hire a Husky</h1>
-  <div id="search-box">
-  	<input type="text" id="welcome_search_box" class="form-control" placeholder="Search to get started">
-  </div>
+		<div style="position: relative;
+			margin-left: 0px; margin-right: 18px; margin-bottom: 0px; margin-top: 0px;
+			padding: 1px;
+			height: 100%;
+			width: 400px;
+			background-color: #EAE6D4;
+			float: left;">
+		<input type="text" id="welcome_search_box" class="form-control" placeholder="Search for Job">
+		</div>
+
+		<div style="position: relative;
+			margin: 0;
+			padding: 1px;
+			height: 100%;
+			width: 200px;
+			background-color: #EAE6D4;
+			float: left;">
+		<input type="text" id="welcome_search_box" class="form-control" placeholder="Search a Location ">
+		</div> 
+
+  
+
+
+
   <br>
   <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
 </div>
